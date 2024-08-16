b4bc670de642c78d89dc8bb19276b925e0ac111b
Karthik MC <161485235+karthikmc-git@users.noreply.github.com>
2024-08-16 20:40:12 +0530

    Add files via upload

---
+++ morse_code_converter/main.py
@@ -0,0 +1,62 @@
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+def encrypt(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+            cipher += ' '
+    return cipher
+
+def decrypt(message):
+    message += ' '
+    decipher = ''
+    citext = ''
+    i = 0
+    for letter in message:
+        if (letter != ' '):
+            i = 0
+            citext += letter
+        else:
+            i += 1
+            if i == 2:
+                decipher += ' '
+            elif citext:
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT.values()).index(citext)]
+                citext = ''
+    return decipher
+
+def main():
+    choice = input("Type '1' to convert string to Morse code, '2' to convert Morse code to string: ")
+    
+    if choice == '1':
+        message = input("Enter the string to convert to Morse code: ").upper()
+        result = encrypt(message)
+        print(f"Morse Code: {result}")
+        
+    elif choice == '2':
+        message = input("Enter the Morse code to convert to string: ")
+        result = decrypt(message)
+        print(f"Decrypted String: {result}")
+        
+    else:
+        print("Invalid choice. Please choose '1' or '2'.")
+
+if __name__ == '__main__':
+    main()
+1
